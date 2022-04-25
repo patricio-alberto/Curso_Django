885e527113bfdc35bee00a43065a2c1b6887e79d
patricio alberto <patricioalbertocostadesousa@gmail.com>
2022-04-24 21:41:51 -0300

    Configurações e renderização de templates

--- porteiros/models.py
+++ porteiros/models.py
@@ -1,6 +1,14 @@
+from tabnanny import verbose
 from django.db import models
 
 class Porteiro(models.Model):
+    
+    usuario = models.OneToOneField(
+        "usuarios.Usuario",
+        verbose_name="Usuario",
+        on_delete=models.PROTECT,
+    )
+
     nome_completo = models.CharField(
         verbose_name= "Nome Completo",
         max_length= 194,
@@ -15,3 +23,17 @@ class Porteiro(models.Model):
         verbose_name= "Telefone de contato",
         max_length= 11,
     )
+
+    data_nascimento = models.DateField(
+        verbose_name= "Data de nascimento",
+        auto_now= False,
+        auto_now_add= False,
+    )
+
+    class Meta:
+        verbose_name = "Porteiro"
+        verbose_name_plural = "Porteiros"
+        db_table = "porteiro"
+
+    def __str__(self):
+        return self.nome_completo
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá, mundo!")
+    return render(request, "index.html")
